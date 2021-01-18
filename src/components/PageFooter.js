2623aec533ecddd2478173363e7d974a41bf4fb5
Ben <b.roullet@skilvioo.com>
2021-01-18 12:00:01 +0100

    change email

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -4,7 +4,8 @@ export default function PageFooter() {
   return (
     <div id="footer">
       <ul className="copyright">
-        <li>&copy; 2021. All rights reserved.</li>
+        <li> contact@fant-home.fr</li>
+        <li>&copy; 2021. FANT'HOME SAS - All rights reserved</li>
         {/* <li>
           Design: <a href="http://html5up.net">HTML5 UP</a>
         </li>
